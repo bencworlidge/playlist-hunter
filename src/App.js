2335d894dd317b80980ec660b775112b7a7619fc
Ben Worlidge <worlidge@Bens-MacBook-Pro.local>
2020-10-29 14:10:55 +0000

    further styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react'
 import './App.css';
-import {Spotify, getTrackFeatures, getTrackAnalysis} from './util/Spotify'
+import {Spotify, getTrackFeatures, getTrackAnalysis, getTrackKey} from './util/Spotify'
+import Background from './background.mp4'
 
 /* TO DO:
 1. Button For Log In 
-2. Look up .bind
+2. Look up .bind 
+3. Phone compability
+4. Color Scheme / Layout
+7. Look up CCS Modules in React https://create-react-app.dev/docs/adding-a-css-modules-stylesheet/
+8. Filter number of results
+11. Fix weird white thing
+12. Loading Positioning
+13. Refresh Search
+14. Not returning all keys
 */
 
 class SearchBar extends Component {
   constructor() {
     super()
-
+    this.state = {
+      searchQuery: ''
+    }
     this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
   }
 
   search() {
-    this.props.onSearch(this.state.searchQuery)
+    this.state.searchQuery ? this.props.onSearch(this.state.searchQuery)
+    : alert('Please Enter A Track Title')
   }
 
   handleTermChange(event) {
@@ -25,27 +37,33 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <div className='searchbar'>
-        <h2>Track For Analysis:</h2>
-        <input 
-          type="text" 
-          placeholder="Track Title..." 
-          onChange={this.handleTermChange}
-        />
-        <button 
-          onClick={this.search}>
-        Search
-        </button>
+      <div>
+        <div className='searchbar'>
+          <h2>Track For Analysis:</h2>
+          <input 
+            type="text" 
+            placeholder="Track Title..." 
+            onChange={this.handleTermChange}
+          />
+          <br></br>
+          <button 
+            onClick={this.search}>
+          SEARCH
+          </button>
+        
+        </div>
       </div>
+      
     )
   }
 
 }
 
 class SearchResults extends Component {
-
+  
   render() {
     return (
+      
       <div className="searchresults">
         {this.props.tracks.map((track, i) => {
           return (
@@ -58,7 +76,6 @@ class SearchResults extends Component {
       </div>
     )
   }
-  
 }
 
 class Track extends Component {
@@ -82,21 +99,25 @@ class Track extends Component {
   render() {
     return (
       
-      <div className="track">
+      <div className="track" >
         <p><strong>{this.props.track.name}</strong> - {this.props.track.artist}</p>
+        {this.state.isActive ?
         <button
-          onClick={() => this.handleClick()}>
-          Show Analysis
+          onClick={() => this.closeAnalysis()}>
+          Close Analysis <i class="fa fa-plus close"></i> 
+        </button> :
+        <button
+          onClick={() =>  this.handleClick()}>
+          Show Analysis <i class="fas fa-plus "></i> 
         </button>
+        }
         {this.state.isActive ? 
           <div className="analysis">
             <Analysis 
               trackId={this.state.trackId}
-              onClose={this.closeAnalysis.bind(this)}
             />
           </div> : null
         }
-          
       </div>
     )
   }
@@ -108,38 +129,35 @@ class Analysis extends Component {
     this.state = {
       audioAnalysis: '',
       audioFeatures: '',
+      trackKey: ''
     }
   }
 
   async componentDidMount() {
-      this.setState({
-        audioAnalysis: await getTrackAnalysis(this.props.trackId),
-        audioFeatures:  await getTrackFeatures(this.props.trackId),
-      })
-      
+    this.setState({
+      audioAnalysis: await getTrackAnalysis(this.props.trackId),
+      audioFeatures:  await getTrackFeatures(this.props.trackId),
+      trackKey: await getTrackKey(this.props.trackId)
+    })    
   }
 
-  componentDidUpdate() {
-    console.log(this.state.audioAnalysis)
-    console.log(this.state.audioFeatures)
-  }
-
-    render() {
-      return (
+  render() {
+    return (
+      <div>
+      {this.state.audioFeatures ?
         <div>
-          {/* <p>tempo: {this.audioFeatures.tempo}</p> */}
-          <p>Length: {this.state.audioFeatures.duration_ms}</p>
-          <p>BPM: {this.state.audioFeatures.tempo}</p>
-          <p>Key: {this.state.audioFeatures.key} {this.state.audioFeatures.mode}</p>
-          <p>Danceability: {this.state.audioFeatures.danceability}</p>
-          <p>Energy: {this.state.audioFeatures.energy}</p>
-          <p>Acousticness {this.state.audioFeatures.acousticness}</p>
-          <button onClick={this.props.onClose}>close</button> 
+            <p>Tempo: {Math.round(this.state.audioFeatures.tempo)} bpm</p>
+            <p>Key: {this.state.trackKey}</p>
+            <p>Danceability: {Math.round(this.state.audioFeatures.danceability * 100)}%</p>
+            <p>Energy: {Math.round(this.state.audioFeatures.energy * 100)}%</p>
+            <p>Acousticness: {Math.round(this.state.audioFeatures.acousticness * 100)}%</p>
         </div>
-      )
-    }
+      : <i class="fa fa-spinner fa-spin fa-3x fa-fw"></i>
+      }
+      </div>
+    )
+  };
 }
-
 
 class App extends Component {
 
@@ -147,7 +165,7 @@ class App extends Component {
     super() 
     this.state = {
       tracks: [],
-      trackId: ''
+      trackId: '',
     }
     this.search = this.search.bind(this);
   }
@@ -156,32 +174,41 @@ class App extends Component {
     Spotify.search(searchQuery).then((searchResults) => {
       this.setState({ tracks: searchResults });
     });
+    this.setState({ searchOn: true });
   }
 
- 
+  
+  
   render() {
-   
     return (
-      
       <div className="App">
+        <video autoPlay loop muted className='video'
+          style={{
+            position: "fixed",
+            width: '100%', 
+            left: '50%',
+            top: '50%', 
+            height: '100%',
+            objectFit: 'cover',
+            transform: 'translate(-50%, -50%)',
+            zIndex: '-1'
+          }}
+        >
+          <source src={Background} type="video/mp4"/>
+        </video>
         <header className="App-header">
-          <h1>Advanced Track Analysis</h1>
+        <h1><i class="fab fa-spotify"></i>  Spotify Track Analysis</h1>
         </header>
         <SearchBar
           onSearch = {this.search}
           searchQuery = {this.state.searchQuery}
         />
-      
         <SearchResults 
           tracks = {this.state.tracks}
-          />
+        />
       </div>
-
     )
-
   };
-
 }
 
-console.log(getTrackAnalysis("4JE6agBLHGA5TaF6FlqfBD"))
 export default App;
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,3 @@
-
 import queryString from 'query-string'
 
 const Spotify = {
@@ -73,8 +72,70 @@ async function getTrackAnalysis(track) {
     return trackAnalysis
 }
 
+async function getTrackKey(track) {
+    const trackKey = await Spotify.features(track)
+    let correctScale = trackKey.key;
+    console.log(trackKey)
+    switch (correctScale) {
+        case 0:
+        correctScale = 'C'
+        break;
+        case 1:
+        correctScale = 'C#'
+        break;
+        case 2:
+        correctScale = 'D'
+        break;
+        case 3:
+        correctScale = 'D#'
+        break;    
+        case 4:
+        correctScale = 'E'
+        break;
+        case 5:
+        correctScale = 'F'
+        break;
+        case 6:
+        correctScale = 'F#'
+        break;
+        case 7:
+        correctScale = 'G'
+        break;
+        case 8:
+        correctScale = 'G#'
+        break;    
+        case 9:
+        correctScale = 'A'
+        break;
+        case 10:
+        correctScale = 'A#'
+        break;
+        case 11:
+        correctScale = 'B'
+        break;
+        default: 
+        correctScale = 'Unknown'
+        return correctScale
+    } 
+
+    let correctMode = trackKey.mode;
+    switch (correctMode) {
+        case 1:
+        correctMode = 'Major'
+        break;
+        case 2:
+        correctMode = 'Minor'
+        break;
+        default:
+        correctMode = 'Unknown'
+        return correctMode
+    }
+    return correctScale + ' ' + correctMode
+}
+
 export {
     Spotify,
     getTrackAnalysis,
     getTrackFeatures,
+    getTrackKey
 }
